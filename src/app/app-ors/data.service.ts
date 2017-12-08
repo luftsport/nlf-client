212706a2d8035b3a7f79bfb9b12524510d0dff4c
Einar Huseby <einar.huseby@gmail.com>
2017-12-08 23:29:50 +0100

    Default message empty

--- src/app/app-ors/data.service.ts
+++ src/app/app-ors/data.service.ts
@@ -4,7 +4,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 
 export class DataService {
-  private messageSource = new BehaviorSubject<string>("default message");
+  private messageSource = new BehaviorSubject<string>("");
 
   currentMessage = this.messageSource.asObservable();
 
