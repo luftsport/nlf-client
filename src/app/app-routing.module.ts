bb1c89987e6cc27aab5cf2699de078d38aead960
Håkon Søgnen <hknsgn@gmail.com>
2017-12-10 09:21:16 +0100

    Use hash location strategy. Also add catch-all routes last

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,23 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppUserTableComponent } from './app-user/app-user-table/app-user-table.component';
 import { AppOrsComponent } from './app-ors/app-ors.component';
-import { AppChildComponent } from './app-ors/app-child/app-child.component';
 import { DummyComponent } from './dummy/dummy.component';
 
-const topRoutes: Routes = [
-{ path: '', redirectTo: '/home', pathMatch: 'full' },
+const appRoutes: Routes = [
 { path: 'home', component: DummyComponent },
 { path: 'users', component: AppUserTableComponent },
 { path: 'ors', component: AppOrsComponent },
-{ path: '**', component: DummyComponent }
+];
+
+const defaultRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: DummyComponent },
 ];
 
 
-let routes : Routes = [].concat(topRoutes)
-  .concat(AppOrsComponent.routes);
+let routes = appRoutes
+  .concat(AppOrsComponent.routes)
+  .concat(defaultRoutes);
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes)],
+  imports: [ RouterModule.forRoot(routes,{ useHash: true })],
   exports: [ RouterModule],
   declarations: []
 })
