2f624a58673bdff83aa79bb687659989442cc817
Einar <einar.huseby@gmail.com>
2020-03-26 18:31:31 +0100

    New version

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,15 +1,15 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "204927f-dirty",
-    "hash": "204927f",
+    "dirty": false,
+    "raw": "8446af3",
+    "hash": "8446af3",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "204927f-dirty",
+    "suffix": "8446af3",
     "semverString": null,
     "version": "0.3.0",
-    "buildtime": "2020-03-03T13:55:46.338Z"
+    "buildtime": "2020-03-26T17:25:40.866Z"
 };
 /* tslint:enable */
