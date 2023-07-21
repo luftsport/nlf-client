b88cf4d0b94dcdad2b500dc696b01cdbfa39ab34
Einar <einar.huseby@gmail.com>
2023-07-21 10:01:33 +0200

    Stop socket io when local

--- src/app/services/socket/socket.service.ts
+++ src/app/services/socket/socket.service.ts
@@ -4,6 +4,7 @@ import { NlfToastService } from 'app/services/toast/toast.service';
 import { NlfAuthSubjectService } from 'app/services/auth/auth-subject.service';
 import { io } from "socket.io-client";
 import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'environments/environment';
 
 
 export interface socketMessage {
@@ -44,91 +45,94 @@ export class NlfSocketService implements OnInit {
 
   ) {
 
-    this.authDataSubject.observableAuthData.subscribe(
-      data => {
-        if (!!data) {
-          if (!this.socket && !!data?.token) {
+    console.log(environment);
+    if (environment._name != 'local') {
+      this.authDataSubject.observableAuthData.subscribe(
+        data => {
+          if (!!data) {
+            if (!this.socket && !!data?.token) {
 
-            // Subscribe to socket 
-            console.log('Subscribing to backend socket server');
-            //this.socket = io('/', { query: { token: data.token } });
-            this.socket = io('/', {auth: {token: data.token}});
+              // Subscribe to socket 
+              console.log('Subscribing to backend socket server');
+              //this.socket = io('/', { query: { token: data.token } });
+              this.socket = io('/', { auth: { token: data.token } });
 
-            //this.alertService.success('Connected to socket.io for two-way communication');
+              //this.alertService.success('Connected to socket.io for two-way communication');
 
-            // Join rooms!
-            // this.socket.emit("join_room", "obsreg");
-            // this.socket.emit("join_room", "obsreg_motorfly_655"); // Everything for the current obsreg!
-            // this.socket.emit("join_room", "motorfly"); // Seksjon
-            // this.socket.emit("join_room", "notifications"); // Own notifications (allerede i egen kanal...)
-            // this.socket.emit("get_rooms");
-            // Handle incoming actions
+              // Join rooms!
+              // this.socket.emit("join_room", "obsreg");
+              // this.socket.emit("join_room", "obsreg_motorfly_655"); // Everything for the current obsreg!
+              // this.socket.emit("join_room", "motorfly"); // Seksjon
+              // this.socket.emit("join_room", "notifications"); // Own notifications (allerede i egen kanal...)
+              // this.socket.emit("get_rooms");
+              // Handle incoming actions
 
-            // Generic message
-            this.socket.on('message', (message) => {
-              this.showToast(message);
-            });
+              // Generic message
+              this.socket.on('message', (message) => {
+                this.showToast(message);
+              });
 
-            // Special events:
-            this.socket.on('disconnect', (message) => {
-              this.showToast(message);
-            });
+              // Special events:
+              this.socket.on('disconnect', (message) => {
+                this.showToast(message);
+              });
 
-            // Message only to self!
-            this.socket.on('message_self', (message) => {
-              this.showToast(message);
-            });
+              // Message only to self!
+              this.socket.on('message_self', (message) => {
+                this.showToast(message);
+              });
 
-            // Message to a joined room
-            this.socket.on('message_room', (message) => {
-              this.showToast(message);
-            });
+              // Message to a joined room
+              this.socket.on('message_room', (message) => {
+                this.showToast(message);
+              });
 
-            // Message to do action @TODO needs confirmation dialogue
-            this.socket.on('action', (message) => {
-              this.showToast(message);
-              if (message.hasOwnProperty('link')) {
+              // Message to do action @TODO needs confirmation dialogue
+              this.socket.on('action', (message) => {
+                this.showToast(message);
+                if (message.hasOwnProperty('link')) {
 
-                //let alert = "<strong>${message['title']}</strong>&nbsp;${message['message']}<br /><a>${message['link']}</a>";
+                  //let alert = "<strong>${message['title']}</strong>&nbsp;${message['message']}<br /><a>${message['link']}</a>";
 
-                //this.alertService[message['style']](alert, true, true, 0);
+                  //this.alertService[message['style']](alert, true, true, 0);
 
-              }
-            });
-
-
-            // Message from notification daemon
-            this.socket.on('notification', (message) => {
-              this.showToast(message);
-            });
-
-            // Message from notification daemon
-            this.socket.on('lungo', (message) => {
-              this.showToast(message);
-            });
+                }
+              });
 
 
-            /** Catch-all
-            this.socket.onAny((eventName, ...args) => {
-              console.log('ANY', eventName, args);
-            });
-            
+              // Message from notification daemon
+              this.socket.on('notification', (message) => {
+                this.showToast(message);
+              });
 
-            this.socket.timeout(5000).emit("message", "acknowledge", (err, responses) => {
-              if (err) {
-                // some clients did not acknowledge the event in the given delay
-                console.log('ACC ERR', err);
-              } else {
-                console.log('ACC OK', responses); // one response per client
-              }
-            });
-            **/
+              // Message from notification daemon
+              this.socket.on('lungo', (message) => {
+                this.showToast(message);
+              });
 
+
+              /** Catch-all
+              this.socket.onAny((eventName, ...args) => {
+                console.log('ANY', eventName, args);
+              });
+              
+  
+              this.socket.timeout(5000).emit("message", "acknowledge", (err, responses) => {
+                if (err) {
+                  // some clients did not acknowledge the event in the given delay
+                  console.log('ACC ERR', err);
+                } else {
+                  console.log('ACC OK', responses); // one response per client
+                }
+              });
+              **/
+
+            }
           }
-        }
-      },
-      err => console.log('Problem getting token: ', err)
-    );
+        },
+        err => console.log('Problem getting token: ', err)
+      );
+    }
   }
 
   ngOnInit() {
